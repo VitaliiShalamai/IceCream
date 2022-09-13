7e4eb6add29265d5140a3ab79eda0e79290687c2
VitaliiShalamai <95576622+VitaliiShalamai@users.noreply.github.com>
2022-09-13 03:33:59 +0000

    Deploying to gh-pages from @ VitaliiShalamai/IceCream@d32ae04c48d3df77ee28558b3d8ee5cdada1ceff 🚀

---
+++ index.b5760b07.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-open]"),openModal:document.querySelector("[data-mob-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.openModal.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.b5760b07.js.map
